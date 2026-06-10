0a20a9bfc46302c0ec3e00528b3c4bc025e475fd
Hernan Bou <92617626+hernanbou@users.noreply.github.com>
2026-06-10 12:13:18 -0300

    Delete src directory

--- src/app/core/constants/storage-keys.ts
+++
@@ -1,7 +0,0 @@
-export const STORAGE_KEYS = {
-    products: 'sneaker-store-products',
-    productsVersion: 'sneaker-store-products-version',
-    users: 'sneaker-store-users',
-    cart: 'sneaker-store-cart',
-    currentUser: 'sneaker-store-current-user',
-} as const;
--- src/app/core/models/cart-item.model.ts
+++
@@ -1,9 +0,0 @@
-export interface CartItem {
-  id: string;
-  productId: number;
-  name: string;
-  image: string;
-  price: number;
-  selectedSize: number;
-  quantity: number;
-};
